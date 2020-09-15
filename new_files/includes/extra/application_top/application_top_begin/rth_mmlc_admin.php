49f231b40eba28f8a49cad52f04e4140a7e0cf6d
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-16 00:42:47 +0200

    feat: add feature logs off the MMLC through the module "MMLC im Admin" (#4)
    * Update rth_mmlc_admin.php
    
    Fix: Link to MMLC
    
    * Create logout_mmlc.php
    
    add feature logout MMLC by shop
    
    * Update rth_mmlc_admin.php
    
    * Update and rename logout_mmlc.php to rth_mmlc_admin.php

---
+++ new_files/includes/extra/application_top/application_top_begin/rth_mmlc_admin.php
@@ -0,0 +1,12 @@
+<?php
+// logs off the MMLC through the module "MMLC im Admin"
+switch(basename($PHP_SELF)) {
+
+	case FILENAME_LOGOFF:
+            $stdSessionId = $_COOKIE['PHPSESSID'];
+            session_id($stdSessionId);
+            session_start();
+            $_SESSION['accessRight'] = false;
+            session_write_close();
+    		break;
+}
