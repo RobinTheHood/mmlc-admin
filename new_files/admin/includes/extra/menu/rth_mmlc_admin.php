87ce583848a4e6f8c10ba8fe6c78d8953c74f55e
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-08-03 14:48:05 +0200

    fix: remove php notices

--- new_files/admin/includes/extra/menu/rth_mmlc_admin.php
+++ new_files/admin/includes/extra/menu/rth_mmlc_admin.php
@@ -1,28 +1,23 @@
 <?php
-defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
+
+defined('_VALID_XTC') or die('Direct Access to this location is not allowed.');
 
 if (!defined(MODULE_RTH_MMLC_ADMIN_STATUS) || MODULE_RTH_MMLC_ADMIN_STATUS != 'true') {
     return;
 }
 
-// Sprachabhaengiger Menueeintrag, kann fuer weiter Sprachen ergaenzt werden
-switch ($_SESSION['language_code']) {
-    case 'de':
-        define('MENU_NAME_RTH_MMLC_ADMIN', 'Modified Module Loader Client (MMLC)');
-        break;
-    case 'en':
-        define('MENU_NAME_RTH_MMLC_ADMIN', 'Modified Module Loader Client (MMLC)');
-        break;
-    default:
-        define('MENU_NAME_RTH_MMLC_ADMIN', 'Modified Module Loader Client (MMLC)');
-        break;
-}
+$fwBoxName = [
+    'de' => 'Modified Module Loader Client (MMLC)',
+    'en' => 'Modified Module Loader Client (MMLC)',
+];
+$fwLanguageCode = $_SESSION['language_code'] ?? '';
+$fwSelectedBoxName = $fwBoxName[$fwLanguageCode] ?? $fwBoxName['de'];
 
 //BOX_HEADING_TOOLS = Name der box in der der neue Menueeintrag erscheinen soll
 $add_contents[BOX_HEADING_MODULES][] = [
     'admin_access_name' => 'rth_mmlc_admin',    // Eintrag fuer Adminrechte
     'filename' => 'rth_mmlc_admin.php',         // Dateiname der neuen Admindatei
-    'boxname' => MENU_NAME_RTH_MMLC_ADMIN,      // Anzeigename im Menue
+    'boxname' => $fwSelectedBoxName,            // Anzeigename im Menue
     'parameter' => '',                          // zusaetzliche Parameter z.B. 'set=export'
     'ssl' => ''                                 // SSL oder NONSSL, kein Eintrag = NONSSL
 ];
